7c6e15c50702cfcbaf0c01b4628e5793ed4587f4
katredford <katharine.redford@gmail.com>
2022-08-01 00:20:14 -0500

    first commit

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+// 1) we want to click a button and have the value of that buttion
+// appear in the text area
+
+//2) we want to click an operator and keep it highlighted and call 
+//that operator in a function
+
+//3) we want to click a second number button and have it show the result
+//of the first number operator and second number in text area
+
+//4) clear button should exist because we want to be able to do more operations
+//on the result of the previous operation
